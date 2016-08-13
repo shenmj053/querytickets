194c872095d23d4d4e1d636f0ffd06709c94ae96
shenmj053 <shenmj053@163.com>
2016-08-13 16:40:53 +0800

    add simple test

---
+++ test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import requests
+
+
+class APITestCase(unittest.TestCase):
+    def test_zd_tickets(self):
+        url = 'http://localhost:5000/zd?purpose_codes=ADULT&Date=2016-08-13&from=北京&to=杭州'
+        r = requests.get(url)
+        self.assertTrue(r.status_code == 200)
+
+    def test_hc_tickets(self):
+        url = 'http://localhost:5000/hc?purpose_codes=ADULT&Date=2016-08-15&from=北京&to=杭州&change=上海'
+        r = requests.get(url)
+        self.assertTrue(r.status_code == 200)
+
+if __name__ == '__main__':
+    unittest.main()
