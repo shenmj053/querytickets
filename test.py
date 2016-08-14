bb7c3681d46592e57b41d09a41d7bea3b66e652a
shenmj053 <shenmj053@163.com>
2016-08-14 14:42:35 +0800

    add a test for outdated date time

--- test.py
+++ test.py
@@ -6,19 +6,24 @@ import requests
 
 class APITestCase(unittest.TestCase):
     def test_zd_tickets(self):
-        url = 'http://localhost:5000/zd?purpose_codes=ADULT&Date=2016-08-13&from=北京&to=杭州'
+        url = 'http://localhost:5000/zd?Date=2016-08-13&from=北京&to=杭州'
         r = requests.get(url)
         self.assertTrue(r.status_code == 200)
 
     def test_hc_tickets(self):
-        url = 'http://localhost:5000/hc?purpose_codes=ADULT&Date=2016-08-15&from=北京&to=杭州&change=上海'
+        url = 'http://localhost:5000/hc?Date=2016-08-15&from=北京&to=杭州&change=上海'
         r = requests.get(url)
         self.assertTrue(r.status_code == 200)
 
     def test_error(self):
-        url = 'http://localhost:5000/hc?purpose_codes=ADULT&Date=2016-08-15&from=beijing&to=hangzhou&change=shanghai'
+        url = 'http://localhost:5000/hc?Date=2016-08-15&from=beijing&to=hangzhou&change=shanghai'
         r = requests.get(url)
         self.assertTrue(r.status_code == 400)
+
+    def test_date(self):
+        url = 'http://localhost:5000/hc?Date=2016-07-15&from=北京&to=杭州&change=上海'
+        r = requests.get(url)
+        self.assertTrue(r.status_code == 404)
 
 if __name__ == '__main__':
     unittest.main()
